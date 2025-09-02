b7bbb3992cc7803ea91b60b5de4d623491aa75bf
alandjebbari-cpu <alandjebbari@gmail.com>
2025-09-02 20:16:12 +0300

    Update _app.js

--- _app.js
+++ _app.js
@@ -1,12 +1,33 @@
-import '../styles/globals.css'
-import { Navbar } from '../components/Navbar'
-import { Footer } from '../components/Footer'
+import '../globals.css'
+import { useState, useEffect } from 'react'
+
 export default function App({ Component, pageProps }) {
+  const [lang, setLang] = useState('ar') // لغتك الافتراضية (عربي)
+
+  useEffect(() => {
+    // حتى نخلي اللغة تنحفظ بالمخزن المحلي
+    const savedLang = localStorage.getItem('lang')
+    if (savedLang) {
+      setLang(savedLang)
+    }
+  }, [])
+
+  const switchLang = (newLang) => {
+    setLang(newLang)
+    localStorage.setItem('lang', newLang)
+  }
+
   return (
-    <div className='min-h-screen flex flex-col'>
-      <Navbar/>
-      <main className='flex-1 p-4'><Component {...pageProps}/></main>
-      <Footer/>
+    <div dir={lang === 'ar' || lang === 'ku' ? 'rtl' : 'ltr'}>
+      {/* زرار لتغيير اللغة */}
+      <div style={{ padding: '10px' }}>
+        <button onClick={() => switchLang('ar')}>🇸🇦 عربي</button>
+        <button onClick={() => switchLang('ku')}>🇹🇯 کوردی</button>
+        <button onClick={() => switchLang('en')}>🇬🇧 English</button>
+      </div>
+
+      {/* تحميل الصفحات */}
+      <Component {...pageProps} lang={lang} />
     </div>
   )
 }
