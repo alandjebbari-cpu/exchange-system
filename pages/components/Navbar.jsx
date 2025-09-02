23da30565348004fe38874d07f0036bc4004ee95
alandjebbari-cpu <alandjebbari@gmail.com>
2025-09-02 20:20:11 +0300

    Create Navbar.jsx

---
+++ pages/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export function Navbar(){
+  return (
+    <nav style={{background:"#2563eb", color:"white", padding:"10px"}}>
+      Exchange System | Home | Customers | Rates | Ledger
+    </nav>
+  );
+}
