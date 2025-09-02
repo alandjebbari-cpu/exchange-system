3c5e1a21dc0f858648436e197e55cd506364274e
alandjebbari-cpu <alandjebbari@gmail.com>
2025-09-02 20:22:21 +0300

    Update and rename Footer.jsx to components/Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer(){
+  return (
+    <footer style={{background:"#f3f4f6", padding:"10px", textAlign:"center"}}>
+      © {new Date().getFullYear()} Exchange System
+    </footer>
+  );
+}
