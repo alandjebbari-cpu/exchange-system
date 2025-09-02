8d7d4f8b86aa9b3f5f12f66fce758fdd2280a8d3
alandjebbari-cpu <alandjebbari@gmail.com>
2025-09-03 00:41:27 +0300

    Delete Navbar.jsx

--- Navbar.jsx
+++
@@ -1,2 +0,0 @@
-import Link from 'next/link'
-export function Navbar(){return <nav className='bg-blue-600 text-white p-4 flex justify-between'><div>Exchange</div><div className='space-x-3'><Link href='/'>Home</Link><Link href='/rates'>Rates</Link><Link href='/customers'>Customers</Link><Link href='/ledger'>Ledger</Link></div></nav>}
