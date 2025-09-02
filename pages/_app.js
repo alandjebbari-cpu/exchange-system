a1897dcc2482d78eb22eaf88516ad278eab295b8
alandjebbari-cpu <alandjebbari@gmail.com>
2025-09-02 23:01:04 +0300

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,25 @@
+import '../styles/globals.css'
+import { useState, useEffect } from 'react'
+
+export default function App({ Component, pageProps }) {
+  const [lang, setLang] = useState('ar') // اللغة الافتراضية
+
+  useEffect(() => {
+    // نخلي اللغة محفوظة بالمخزن المحلي
+    const savedLang = localStorage.getItem('lang')
+    if (savedLang) {
+      setLang(savedLang)
+    }
+  }, [])
+
+  const switchLang = (newLang) => {
+    setLang(newLang)
+    localStorage.setItem('lang', newLang)
+  }
+
+  return (
+    <div dir={lang === 'ar' ? 'rtl' : 'ltr'}>
+      <Component {...pageProps} switchLang={switchLang} lang={lang} />
+    </div>
+  )
+}
